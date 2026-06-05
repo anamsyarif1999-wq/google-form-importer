0f5d7ef20db383527da7c9969e42875455ea933f
anamsyarif1999-wq <anamsyarif1999@gmail.com>
2026-06-05 14:20:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # GANTI DENGAN URL FORMRESPONSE FORM BARU
-FORM_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdYY2hbRlhrCY_a06uH0keEsBBu8x6P3AzpZ2BmcmVERjaxpQ/formResponse"
+FORM_URL = "https://docs.google.com/forms/d/1_ZpgRF-hLw7Q2UYtsQUhC6NHiTdO3mVsoAWBxRODrgw/viewform?edit_requested=true"
 
 st.title("Excel ➜ Google Form Importer")
 
